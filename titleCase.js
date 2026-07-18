7b078d85b2a1155ae00580b67fe9d1bf0c92cb2e
Sayed <muhammadsaye741@gmail.com>
2026-07-19 00:09:01 +0600

    titleCase

---
+++ titleCase.js
@@ -0,0 +1,7 @@
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(word => 
+    word.charAt(0).toUpperCase() + word.slice(1)
+  ).join(' ');
+}
+
+console.log(titleCase("i'm a little teapot")); 

5446264c1489078ae380cac508604b426168c28f
Sayed <muhammadsaye741@gmail.com>
2026-07-20 00:08:52 +0600

    formatTime

---
+++ formatTime.js
@@ -0,0 +1,8 @@
+function formatTime(seconds) {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hrs}:${mins}:${secs}`;
+}
+
+console.log(formatTime(3665)); 

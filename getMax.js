fde8a0c140478c01fde441c1cfac2c27c71f8781
Sayed <muhammadsaye741@gmail.com>
2026-07-28 13:01:40 +0600

    getMax

---
+++ getMax.js
@@ -0,0 +1,7 @@
+function getMax(num1, num2) {
+  // Math.max ফাংশন ব্যবহার করে দুটি সংখ্যার মধ্যে বড় সংখ্যাটি বের করা হলো
+  return Math.max(num1, num2);
+}
+
+console.log(getMax(10, 25)); // Output: 25
+console.log(getMax(42, 7));  // Output: 42

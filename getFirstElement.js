15ea07b915abbcbc30aa0d2322f6c6ff29b91085
Sayed <muhammadsaye741@gmail.com>
2026-08-02 10:49:45 +0600

    getFirstElement

---
+++ getFirstElement.js
@@ -0,0 +1,7 @@
+function getFirstElement(arr) {
+  // অ্যারের প্রথম উপাদানটি (ইনডেক্স ০) রিটার্ন করা হলো
+  return arr[0];
+}
+
+console.log(getFirstElement([10, 20, 30])); // Output: 10
+console.log(getFirstElement([]));          // Output: undefined

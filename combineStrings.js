a92b79e87e33e27e92c6d5e52d917b4bdca0b74f
Sayed <muhammadsaye741@gmail.com>
2026-08-02 10:50:47 +0600

    combineStrings

---
+++ combineStrings.js
@@ -0,0 +1,7 @@
+function combineStrings(str1, str2) {
+  // দুটি স্ট্রিংয়ের মাঝখানে একটি স্পেস দিয়ে যুক্ত করা হলো
+  return str1 + " " + str2;
+}
+
+console.log(combineStrings("Hello", "World")); // Output: "Hello World"
+console.log(combineStrings("JavaScript", "Programming")); // Output: "JavaScript Programming"

da1cdb33ef65efcc8a3c10c36f342bb61c2c92b7
Sayed <muhammadsaye741@gmail.com>
2026-08-04 19:14:17 +0600

    isNegative

---
+++ isNegative.js
@@ -0,0 +1,7 @@
+function isNegative(num) {
+  // সংখ্যাটি ০ এর চেয়ে ছোট কি না তা যাচাই করা হলো
+  return num < 0;
+}
+
+console.log(isNegative(-5)); // Output: true
+console.log(isNegative(3));  // Output: false

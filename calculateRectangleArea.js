60c7f3212fcbe2fd9fa7157436d484f36a58c089
Sayed <muhammadsaye741@gmail.com>
2026-08-04 19:15:11 +0600

    calculateRectangleArea

---
+++ calculateRectangleArea.js
@@ -0,0 +1,7 @@
+function calculateRectangleArea(length, width) {
+  // আয়তক্ষেত্রের ক্ষেত্রফল বের করতে দৈর্ঘ্য এবং প্রস্থ গুণ করা হলো
+  return length * width;
+}
+
+console.log(calculateRectangleArea(5, 10)); // Output: 50
+console.log(calculateRectangleArea(3, 7));  // Output: 21

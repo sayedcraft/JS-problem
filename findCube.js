583bc1fd27cc6f80cfda59a8f0aa88c80bcd0a8b
Sayed <muhammadsaye741@gmail.com>
2026-08-05 22:22:39 +0600

    findCube

---
+++ findCube.js
@@ -0,0 +1,7 @@
+function findCube(num) {
+  // সংখ্যাটিকে তিনবার নিজেদের মধ্যে গুণ করে কিউব বা ঘন বের করা হলো
+  return num * num * num;
+}
+
+console.log(findCube(3)); // Output: 27
+console.log(findCube(4)); // Output: 64

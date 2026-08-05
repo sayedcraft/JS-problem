5a71417fbda766ab8fcd1d53765a8a11972970ad
Sayed <muhammadsaye741@gmail.com>
2026-08-05 22:23:11 +0600

    ageToDays

---
+++ ageToDays.js
@@ -0,0 +1,7 @@
+function ageToDays(age) {
+  // বয়সকে ৩৬৫ দিয়ে গুণ করে মোট দিন বের করা হলো
+  return age * 365;
+}
+
+console.log(ageToDays(20)); // Output: 7300
+console.log(ageToDays(1));  // Output: 365

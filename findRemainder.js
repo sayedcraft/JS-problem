9b9e426f3cde0a01763a72e7bff9e4f3f8f2d1cd
Sayed <muhammadsaye741@gmail.com>
2026-08-07 22:42:11 +0600

    findRemainder

---
+++ findRemainder.js
@@ -0,0 +1,7 @@
+function findRemainder(num1, num2) {
+  // মডুলাস (%) অপারেটর ব্যবহার করে ভাগশেষ বা রিমেইন্ডার বের করা হলো
+  return num1 % num2;
+}
+
+console.log(findRemainder(10, 3)); // Output: 1
+console.log(findRemainder(15, 4)); // Output: 3

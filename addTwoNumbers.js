4d121ed02d9f28e71cc9b48d7b8bf8505358a2d7
Sayed <muhammadsaye741@gmail.com>
2026-08-08 18:11:40 +0600

    addTwoNumbers

---
+++ addTwoNumbers.js
@@ -0,0 +1,7 @@
+function addTwoNumbers(num1, num2) {
+  // দুটি সংখ্যা যোগ করে তার ফলাফল রিটার্ন করা হলো
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(5, 7));  // Output: 12
+console.log(addTwoNumbers(-3, 10)); // Output: 7

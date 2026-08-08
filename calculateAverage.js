150f6e1f1698cf630ce2c44a645b54064d2a1a0c
Sayed <muhammadsaye741@gmail.com>
2026-08-08 18:12:58 +0600

    calculateAverage

---
+++ calculateAverage.js
@@ -0,0 +1,7 @@
+function calculateAverage(num1, num2, num3) {
+  // তিনটি সংখ্যার যোগফলকে ৩ দিয়ে ভাগ করে গড় বের করা হলো
+  return (num1 + num2 + num3) / 3;
+}
+
+console.log(calculateAverage(10, 20, 30)); // Output: 20
+console.log(calculateAverage(5, 10, 15));  // Output: 10
